65f8698b19e19894d78db9764427627a250d1459
Duane <dev.duane@gmail.com>
2018-10-07 22:59:07 -0700

    Updated index and package.json to work with Heroku

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
+//If the ENV variable PORT isn't defined, use 8080
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
 	res.send({'hi': 'there'});
-}).listen(8080);
+}).listen(PORT);
