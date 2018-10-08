80de74d8e7620f62071f65e077ba39a2212d2fad
unknown <dev.duane@gmail.com>
2018-10-08 16:38:18 -0700

    server folder gone

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-//If the ENV variable PORT isn't defined, use 8080
-const PORT = process.env.PORT || 8080;
-
-app.get('/', (req, res) => {
-	res.send({'hi': 'there'});
-}).listen(PORT);
