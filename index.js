6d71d35f40f71d5a2833d0a26c5abfc2dd737348
unknown <dev.duane@gmail.com>
2018-10-18 22:06:58 -0700

    Modified index.js

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 
 const app = express();
-const credentials = require('./config/keys');
+const GOOGLE_CLIENT_ID = keys.googleClientID;
+const GOOGLE_CLIENT_SECRET = keys.googleClientSecret;
 
 const googleStrategy = new GoogleStrategy({
-	clientID: GOOGLE_CLIENT_ID,
-	clientSecret: GOOGLE_CLIENT_SECRET,
-	callbackURL: 'http://localhost/'
-});
+		clientID: GOOGLE_CLIENT_ID,
+		clientSecret: GOOGLE_CLIENT_SECRET,
+		callbackURL: '/auth/google/callback'
+	},
+	function(accessToken, refreshToken, profile, cb) {
+		console.log(accessToken);
+	},
+);
 
 passport.use(
 	googleStrategy,
-	function(accessToken, refreshToken, profile, cb) {
-		User.findOrCreate({ googleId: profile.id }, function(err, user) {
-			return cb(err, user);
-		});
-	}
 );
+
+// passport.use(
+// 	new GoogleStrategy({
+// 		clientID: GOOGLE_CLIENT_ID,
+// 		clientSecret: GOOGLE_CLIENT_SECRET,
+// 		callbackURL: '/auth/google/callback'
+// 	},
+// 	function(accessToken, refreshToken, profile, cb) {
+// 		// User.findOrCreate({ googleId: profile.id }, function(err, user) {
+// 		// 	return cb(err, user);
+// 		// });
+// 		// console.log(cb);
+// 		console.log(accessToken);
+// 	}
+// ));
 
 //If the ENV variable PORT isn't defined, use 8080
 const PORT = process.env.PORT || 8080;
@@ -26,6 +43,18 @@ const PORT = process.env.PORT || 8080;
 app.get('/', (req, res) => {
 	res.send({'hi': 'there'});
 });
+
+app.get(
+	'/auth/google', 
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
+
+// app.get('/auth/google/callback', (req, res) => {
+// 	console.log('zzzzzzzzzzzzzz');
+// 	res.send({'hi': 'there'});
+// });
 
 app.listen(PORT);
 
