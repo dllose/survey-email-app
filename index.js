33ff9b09b72dd64ab666edd554df174ee33fd70b
unknown <dev.duane@gmail.com>
2018-12-05 22:48:45 -0800

    Added checker for environment variable

--- index.js
+++ index.js
@@ -39,8 +39,12 @@ require('./routes/stripeRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
     // Express runs assets for production (main.js, main.css)
-
+    app.use(express.static('client/build'));
     // if the backend doesn't recognize the route, send back index.html
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 //If the ENV variable PORT isn't defined, use 8080
