a1ab888810ef91d90c5a60d4865328555691d035
Duane <dev.duane@gmail.com>
2018-12-16 17:25:49 -0800

    Added Mailer class

---
+++ services/Mailer.js
@@ -0,0 +1,15 @@
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail;
+const apiKey = require('../config/keys').sendGridKey;
+
+// var Mailer = function() {
+
+// }
+
+// var Mailer = new Object();
+// Mailer.prototype.method_name = helper.Mail;
+
+
+class Mailer extends helper.Mail {
+
+}
