c33cfaaec8281e69ff69f2772deaa51d5a2f3227
unknown <dev.duane@gmail.com>
2018-12-23 11:20:26 -0800

    Added Redux Form

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SurveyNew extends Component {
+
+    render() {
+        return (
+            <div>
+                Survey New!
+            </div>
+        );
+    }
+
+}
+
+export default SurveyNew;
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 
+import { reducer as formReducer } from 'redux-form';
+
 const reducers = {
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 };
 
 export default combineReducers(reducers);
