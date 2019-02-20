f2fcd3217bb2ba5e8b22770947aa83808ab9d063
Duane <dev.duane@gmail.com>
2019-02-20 00:16:32 -0800

    Changed text

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,9 +5,10 @@ const About = () => {
     return (
         <div>
             <div>
-                <p>Application that I use for testing Full-Stack React Application but might considering to release it but would have to create more featuers</p>
                 <p>This is a MERN application (MongoDB, Express, React and Node JS)</p>
                 <p>Users would sign-up using their Google Account via OAuth, no need to sign-up on the site</p>
+                <p>You can load credits using a test credit card after signing-in just use the credit card number "4242 4242 4242 4242"</p>
+                <p>This is still under development</p>
                 <ul class="collection with-header">
                     <li className="collection-header">
                         <h5>People involved with this app:</h5>
@@ -19,7 +20,7 @@ const About = () => {
                     </li>
                     <li className="collection-item avatar">
                         <img src="https://graph.facebook.com/v3.2/100000075774214/picture?type=large" alt="" className="circle" />
-                        <span className="title">Software Tester</span>
+                        <span className="title">Software Quality Assurance</span>
                         <p>Karla S. Mangosong</p>
                     </li>
                 </ul>
